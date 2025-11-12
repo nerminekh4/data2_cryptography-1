266a4a417b62687d6390c9c383410a00600d6ddb
Hakim Horairy <horairyhakim@gmail.com>
2025-11-12 14:05:06 +0100

    refactor: cesar cipher function

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
 import string
 
 def cesar_cipher(text, key):
-	crypted_text = ""
-	
-	for char in text:
-		crypted_text += chr((ord(char) + key) % 1_114_112)
-
-	return crypted_text
-
-def cesar_uncipher(crypted_message, key):
-		return cesar_cipher(crypted_message, -key)
-
-
-def hack_cesar_cipher(crypted_message, alphabet):
-
-	for possible_key in range(0, 1_114_112):
-		possible_uncryption = cesar_uncipher(crypted_message, possible_key)
-		if possible_uncryption[0] in alphabet:
-			print(possible_key)
-			print(possible_uncryption)
-			print("_"*20)
+	if type(text) == str and type(key) == int:
+		return "".join([chr((ord(char) + key) % 1_114_112) for char in text])
+	else:
+		raise(TypeError)
 
 
 
 
+def cesar_uncipher(crypted_text, key):
+		return cesar_cipher(crypted_text, -key)
 
 
-message = "chocolat"
+def hack_cesar_cipher(crypted_text, alphabet):
+	if type(crypted_text) == str and type(alphabet) == str:
+		for possible_key in range(0, 1_114_112):
+			possible_uncryption = cesar_uncipher(crypted_text, possible_key)
+			if possible_uncryption[0] in alphabet:
+				print(possible_key)
+				print(possible_uncryption)
+				print("_"*20)
+	else:
+		raise(TypeError)
 
 
-crypted_message = cesar_cipher(message, 531_200_000) # exo 1
 
-initial_message = cesar_uncipher(crypted_message, 531200000) # exo 2
+
+
+message = "le chocolat est bon"
+
+
+crypted_text = cesar_cipher(message, 12) # exo 1
+print(crypted_text)
+
+initial_message = cesar_uncipher(crypted_text, 12) # exo 2
 print(initial_message == message)
 
-hack_cesar_cipher(crypted_message, alphabet=string.printable) # exo3
+hack_cesar_cipher(crypted_text, alphabet=string.printable) # exo3
 
 
